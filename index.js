c677ba66e698fb36a2f1825b5466352ab98df743
SunriseSystem <daniel199257@gmail.com>
2020-08-31 00:04:02 -0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { route } = require('./route.js')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8080  
+app.use(cors())
 app.use(route)
 app.listen(port, () => console.log('Connect in server', port))
